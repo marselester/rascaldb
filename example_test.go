e5da85522482cc464dd9fa278e9c413a89a65ee5
marselester <marselester@ya.ru>
2018-07-26 09:36:15 +0700

    Add package example test

---
+++ example_test.go
@@ -0,0 +1,29 @@
+package rascaldb_test
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/marselester/rascaldb"
+)
+
+func Example() {
+	db, err := rascaldb.Open("testdata/new.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	name := []byte("Moist von Lipwig")
+	if err = db.Set("name", name); err != nil {
+		log.Fatal(err)
+	}
+
+	name, err = db.Get("name")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s\n", name)
+	// Output:
+	// Moist von Lipwig
+}
